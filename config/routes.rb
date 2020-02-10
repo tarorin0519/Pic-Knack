1e24c308b8a0665f4639fd6058477a5153b3b20f
tarorin0519 <rin.ice.musica0519@gmail.com>
2020-02-10 12:39:11 +0000

    active_adminから管理者作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :admin_accounts, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users, controllers:{
+  	sessions: 'users/sessions',
+  	registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
