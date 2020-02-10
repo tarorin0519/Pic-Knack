e82aac9d371b054964ca7f40099ba403c8c8cb69
tarorin0519 <rin.ice.musica0519@gmail.com>
2020-02-10 12:48:18 +0000

    post,genre,post_comment,favoriteの各model,controller作成　home controller作成,

---
+++ db/migrate/20200210124059_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :body
+      t.string :image_id
+      t.string :genre_id
+      t.string :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_123504) do
+ActiveRecord::Schema.define(version: 2020_02_10_124329) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,37 @@ ActiveRecord::Schema.define(version: 2020_02_10_123504) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "active_flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.string "user_id_integer"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.string "image_id"
+    t.string "genre_id"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
